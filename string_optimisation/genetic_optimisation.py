cf8dad9999057c998cc5147e29a993a24644534b
Linwal <linwal@bluewin.ch>
2018-03-26 13:38:44 +0200

    adapted filepaths

--- string_optimisation/genetic_optimisation.py
+++ string_optimisation/genetic_optimisation.py
@@ -31,10 +31,9 @@ def evaluation_function(individual, amodule_irradiation_np, amodule_area, amodul
     return total_energy_yield/1000.0,
 
 
-def run_optimisation(cxpb=0.9, mutpb=0.5, number_of_generations=1000, population_size=500):
+def run_optimisation(cxpb=0.9, mutpb=0.5, number_of_generations=1000, population_size=500, number_of_strings=None):
 
     random.seed(64)
-
     pop = toolbox.population(n=population_size)
     print "start of evolution"
 
@@ -69,35 +68,32 @@ def run_optimisation(cxpb=0.9, mutpb=0.5, number_of_generations=1000, population
                     toolbox.mutate(mutant)
                     del mutant.fitness.values
 
-
-        # Evaluate the individuals with an invald fitness
+        # Evaluate the individuals with an invalid fitness
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-
         fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
 
         for ind, fit in zip(invalid_ind, fitnesses):
                 ind.fitness.values = fit
         pop[:] = offspring
 
-
-
         # Gather all the fitnesses in one list and print the stats
         generation_fits = [ind.fitness.values[0] for ind in pop]
-
         generation_max = max(generation_fits)
         generation_history.append(generation_max)
 
 
+
         if generation==number_of_generations:
-            history = plt.figure(2)
-            history.clf()
+
+            print aux.decoding(pop[generation_fits.index(generation_max)], number_of_strings)
             plt.plot(generation_history)
-            pd.DataFrame(generation_history).to_csv(r"C:\Users\walkerl\Desktop\temp_stuff\Images\history%s.csv" % (str(int(100*cxpb)) + "_" +str(int(100*mutpb))))
+            result_path = os.path.join(os.path.dirname(__file__), r"data\history.csv")
+            pd.DataFrame(generation_history).to_csv(result_path)
             plt.ylabel("Output [kWh]")
             plt.xlabel("Generations [n]")
             plt.title("Evolution with cxpb = %s and mutpb = %s" %(cxpb, mutpb))
-            plt.savefig(r"C:\Users\walkerl\Desktop\temp_stuff\Images\history%s" % (str(int(100*cxpb)) + "_" +str(int(100*mutpb))))
-
+            # plt.savefig(r"C:\Users\walkerl\Desktop\temp_stuff\Images\history%s" % (str(int(100*cxpb)) + "_" +str(int(100*mutpb))))
+            plt.show()
 
 
 
@@ -112,7 +108,7 @@ creator.create("Individual", list, fitness=creator.FitnessMax)
 if __name__ == "__main__":
 
 
-    # Definitions of parameters
+    ### ========= Definition of parameters =========== ###
     maximum_string_number = 10
     module_area = 0.90  # m2
     sensor_points_per_module = 40
@@ -129,6 +125,8 @@ if __name__ == "__main__":
     mutpb=0.4
     number_of_generations = 1000
     population_size = 500
+
+
 
     # Definitions of filepaths and import of data
     current_directory = os.path.dirname(__file__)
@@ -184,13 +182,7 @@ if __name__ == "__main__":
     #  Choose type of selection algorithm
     toolbox.register("select", tools.selTournament, tournsize=3)
 
-    run_optimisation(cxpb=cxpb, mutpb=mutpb, number_of_generations=number_of_generations, population_size=population_size)
+    run_optimisation(cxpb=cxpb, mutpb=mutpb, number_of_generations=number_of_generations,
+                     population_size=population_size, number_of_strings=maximum_string_number)
 
-
-    # cxpb = [0.01,0.2,0.4,0.6,0.8,0.99]
-    # mutpb = [0.01,0.2,0.4,0.6,0.8,0.99]
-    #
-    # for crossover_prob in cxpb:
-    #     for mutation_pb in mutpb:
-    #         run_optimisation(cxpb=crossover_prob, mutpb=mutation_pb, number_of_generations=number_of_generations)
 
